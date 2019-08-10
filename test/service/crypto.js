89817b2b1e3a5e6db09a05f4438161cf99ce51f1
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2019-08-09 20:42:31 -0600

    [add] Missing test case for secureRandomString()

--- test/service/crypto.js
+++ test/service/crypto.js
@@ -14,4 +14,11 @@ describe('src.service.crypto.secureRandomString()', function() {
     it('Compares the string to the asked length, it should be equal or larger  due to the bin2hex', async function() {
         assert.isAtLeast(data.length, 10);
     });
+
+    it('Requests a random string without passing a length argument,  should get a string of default length', async function() {
+        data = await cryptoService.secureRandomString();
+        assert.isNotNull(data);
+        assert.isString(data);
+        assert.isAtLeast(data.length, 32);
+    });
 });
