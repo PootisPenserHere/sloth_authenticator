eb0f46b9f91b8a76d2be53f8fcab404a3445f1aa
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2019-08-09 20:35:47 -0600

    [add] Crypto service tests

---
+++ test/service/crypto.js
@@ -0,0 +1,17 @@
+const assert = require('chai').assert;
+
+const cryptoService = require('../../src/service/crypto');
+
+describe('src.service.crypto.secureRandomString()', function() {
+    let data;
+
+    it('Verifies that the random output is a string', async function() {
+        data = await cryptoService.secureRandomString(10);
+        assert.isNotNull(data);
+        assert.isString(data);
+    });
+
+    it('Compares the string to the asked length, it should be equal or larger  due to the bin2hex', async function() {
+        assert.isAtLeast(data.length, 10);
+    });
+});
