a0ac566572fa75d2b2410b1d825549f7bba03cba
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2019-08-10 21:31:41 -0600

    [mod] Redis service test separated by method

--- test/service/redis.js
+++ test/service/redis.js
@@ -14,32 +14,50 @@ let ttl = 10;
 let stringValue = 'random value!';
 let objectValue = {'one': 1, 'string': 'thing'};
 
-describe('src.service.redis.readFile()', function() {
-    it('Saves a string in redis, it should have the default ttl', async function() {
+describe('src.service.redis.setKey()', function() {
+    it('Saves a string in redis, it should have the default ttl', async function () {
         let data = await redisService.setKey(keyWithoutTtl, stringValue);
         assert.isNotNull(data);
         assert.equal(data, 'OK'); // Default response on success
+
+        let keyTtl = await redisService.getTtl(keyWithoutTtl);
+        assert.isNotNull(keyTtl);
+        assert.isNumber(keyTtl);
+        assert.isAtMost(keyTtl, parseInt(process.env.REDIS_DEFAULT_TTL));
+        assert.isAtLeast(keyTtl, parseInt(process.env.REDIS_DEFAULT_TTL) - 1);
     });
 
-    it('Reads the ttl of a key saved without sending a ttl, should return an int with the default ttl', async function() {
+    it('Saves a string in redis with a predefined ttl', async function() {
+        let data = await redisService.setKey(keyWithTtl, stringValue, ttl);
+        assert.isNotNull(data);
+        assert.equal(data, 'OK'); // Default response on success
+
+        let keyTtl = await redisService.getTtl(keyWithTtl);
+        assert.isNotNull(keyTtl);
+        assert.isNumber(keyTtl);
+        assert.isAtMost(keyTtl, ttl);
+        assert.isAtLeast(keyTtl, ttl - 1);
+    });
+});
+
+describe('src.service.redis.getTtl()', function() {
+    it('Reads the ttl of a key, should return an int above 0', async function () {
         let data = await redisService.getTtl(keyWithoutTtl);
         assert.isNotNull(data);
         assert.isNumber(data);
         assert.isAtLeast(data, 1);
     });
+});
 
-    it('Saves a string in redis with a ttl', async function() {
-        let data = await redisService.setKey(keyWithTtl, stringValue, ttl);
-        assert.isNotNull(data);
-        assert.equal(data, 'OK'); // Default response on success
-    });
-
+describe('src.service.redis.getKey()', function() {
     it('Reads a key in redis', async function() {
         let data = await redisService.getKey(keyWithTtl);
         assert.isNotNull(data);
         assert.equal(data, stringValue);
     });
+});
 
+describe('src.service.redis.deleteKey()', function() {
     it('Deletes a key in redis', async function() {
         // First ensure that the key is exists
         let verifyingExistence = await redisService.getKey(keyWithTtl);
@@ -56,16 +74,38 @@ describe('src.service.redis.readFile()', function() {
         let verifyingDelete = await redisService.getKey(keyWithTtl);
         assert.isNull(verifyingDelete); // No key should have been found
     });
+});
 
+describe('src.service.redis.setJsonKey()', function() {
     it('Saves an object in redis, it should have the default ttl', async function() {
         let data = await redisService.setJsonKey(keyWithoutTtl, objectValue);
         assert.isNotNull(data);
         assert.equal(data, 'OK'); // Default response on success
+
+        let keyTtl = await redisService.getTtl(keyWithoutTtl);
+        assert.isNotNull(keyTtl);
+        assert.isNumber(keyTtl);
+        assert.isAtMost(keyTtl, parseInt(process.env.REDIS_DEFAULT_TTL));
+        assert.isAtLeast(keyTtl, parseInt(process.env.REDIS_DEFAULT_TTL) - 1);
     });
 
     it('Saves an object in redis with a ttl', async function() {
-        let data = await redisService.setJsonKey(keyWithoutTtl, objectValue, ttl);
+        let data = await redisService.setJsonKey(keyWithTtl, objectValue, ttl);
         assert.isNotNull(data);
         assert.equal(data, 'OK'); // Default response on success
+
+        let keyTtl = await redisService.getTtl(keyWithTtl);
+        assert.isNotNull(keyTtl);
+        assert.isNumber(keyTtl);
+        assert.isAtMost(keyTtl, ttl);
+        assert.isAtLeast(keyTtl, ttl - 1);
+    });
+});
+
+describe('src.service.redis.getJsonKey()', function() {
+    it('Reads an object in redis and compares it to the saved one', async function() {
+        let data = await redisService.getJsonKey(keyWithoutTtl);
+        assert.isNotNull(data);
+        assert.deepEqual(data, objectValue);
     });
 });
