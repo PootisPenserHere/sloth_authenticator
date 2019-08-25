6cdfae3c68edc6d051386cf1e9aed8e407d39b1e
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2019-08-25 02:38:35 -0600

    [add] Pushing commit tagged image

--- scripts/deploy_hash_version_to_docker.sh
+++ scripts/deploy_hash_version_to_docker.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 # Deploys the current build tagged with its commit hash
+SHORT_COMMIT_HASH=$(echo $TRAVIS_COMMIT | cut -c1-7)
+sudo docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$SHORT_COMMIT_HASH
+sudo docker push $REPOSITORY_URI:$SHORT_COMMIT_HASH
